c5f0cec4f7e1ffcf2462dca5febeb06880e9b08c
Muhammad Avtara Khrisna <avtarakhrisna1@gmail.com>
2018-08-25 13:14:31 +0700

    No 2 Seleksi Bootcamp Arkademy #4

---
+++ No 2/field.php
@@ -0,0 +1,68 @@
+<html>
+<head>
+<title>Check</title>
+</head>
+<body>
+
+<table>
+<tr>
+<form method="POST">
+<td>
+<label>Username :</label>
+</td>
+<td>
+<input type="text" name="username" placeholder="Insert Username"> 
+</td>
+</tr>
+<tr>
+<td>
+<label>Email : </label>
+</td>
+<td>
+<input type="email" name="email" placeholder="Insert Password">
+</td>
+</tr>
+
+<tr>
+<td>
+<label>Phone Number :</label>
+</td>
+<td>
+<input type="text" name="phoneNumber" placeholder='Insert Phonenumber'>
+</td>
+</tr>
+<td>	
+<input type="submit" name="submit" value="SUBMIT">
+</td>
+</form>
+</table>
+
+<br>
+
+
+
+<?php
+if(isset($_POST['submit'])){
+	if(ctype_lower($_POST['username'])){
+		echo "Valid Username<br>";
+	}else{
+		echo "<script>alert('Username Must Be Lower');</script>";
+	}
+	if(preg_match("/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix", $_POST['email'])) {
+	  echo 'Valid Email<br>';
+	} else {
+	  echo "<script>alert('Invalid Email');</script>";
+	}
+	if (preg_match("/^[\+0-9\-\(\)\s]*$/", $_POST['phoneNumber'])) {
+		echo "Valid Phone Number<br>";
+	}else{
+		echo "<script>alert('Invalid Phone Number');</script>";
+	}
+}else{
+	echo "Tolong isi Data diatas";
+}
+?>
+
+
+</body>
+</html>
